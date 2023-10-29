a35606b41925b77c0447e617952ef684eca9716e
Aiden Stickney <stickneyaiden@gmail.com>
2023-10-29 02:10:11 -0500

    redis

--- backend/backend/strokes_storage.py
+++ backend/backend/strokes_storage.py
@@ -1,14 +1,38 @@
 # strokes_storage.py
 
-strokes_data = []
+import redis
+
+# Connect to the Redis server
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 def get_strokes():
-    return strokes_data.copy()
+    # Get strokes data from Redis
+    strokes_data = r.lrange('strokes', 0, -1)
+    # Convert bytes to list of lists of floats
+    strokes = [eval(stroke.decode('utf-8')) for stroke in strokes_data]
+    return strokes
 
 def add_stroke(stroke):
-    strokes_data.append(stroke)
-    print(strokes_data)
+    # Add a stroke to the list in Redis
+    r.rpush('strokes', str(stroke))
+
 
 def clear_strokes():
-    strokes_data.clear()
-    print(strokes_data)
+    # Clear the strokes data in Redis
+    r.delete('strokes')
+
+
+# # strokes_storage.py
+
+# strokes_data = []
+
+# def get_strokes():
+#     return strokes_data.copy()
+
+# def add_stroke(stroke):
+#     strokes_data.append(stroke)
+#     print(strokes_data)
+
+# def clear_strokes():
+#     strokes_data.clear()
+#     print(strokes_data)
