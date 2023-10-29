4076ba44ab0faf639f4f478d0e66b36ce741623c
Aiden Stickney <stickneyaiden@gmail.com>
2023-10-29 00:49:03 -0500

    no slash endpoints

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,7 +7,11 @@ from . import views
 urlpatterns = [
     path('', views.handleHome, name='home'),  # handle requests to the root URL '/'
     path('predict/', views.handlePredict, name='predict'),
+    path('predict', views.handlePredict, name='predict-no-slash'),
     path('guess/', views.handleGuess, name='guess'),
+    path('guess', views.handleGuess, name='guess-no-slash'),
     path('newcase/', views.handleNewCase, name='newcase'),
+    path('newcase', views.handleNewCase, name='newcase-no-slash'),
     path('score/', views.handleScore, name='score'),
+    path('score', views.handleScore, name='score-no-slash'),
 ]
